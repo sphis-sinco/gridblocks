71f60a2496a886f6821aaaf4988eaec7d00f6965
Viwu <sinconsistencia@gmail.com>
2025-11-22 06:48:08 -0600

    DebugButtonTesting todo class

--- source/sphis/gridblocks/Gridblocks.hx
+++ source/sphis/gridblocks/Gridblocks.hx
@@ -1,12 +1,15 @@
 package sphis.gridblocks;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import sphis.gridblocks.debug.DebugButtonTesting;
 
 class Gridblocks extends FlxState
 {
 	override public function create()
 	{
 		super.create();
+		FlxG.switchState(() -> new DebugButtonTesting());
 	}
 
 	override public function update(elapsed:Float)
---
+++ source/sphis/gridblocks/debug/DebugButtonTesting.hx
@@ -0,0 +1,8 @@
+package sphis.gridblocks.debug;
+
+import flixel.FlxState;
+
+class DebugButtonTesting extends FlxState
+{
+    
+}
